be492ff0401eabc22ad065092c64db493e0d7f82
SlawomirJJ <sawomirjedziniak@gmail.com>
2021-09-23 21:44:15 +0200

    robienie frontu

---
+++ AstarWeb/Controllers/PoleController.cs
@@ -0,0 +1,24 @@
+﻿using AstarWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AstarWeb.Controllers
+{
+  
+    public class PoleController : Controller
+    {
+        static List<PoleModel> Pola = new List<PoleModel>();
+        public IActionResult Pole()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Pola.Add(new PoleModel { Id = i, Otwarty=true }) ;
+            }
+            
+            return View(Pola);
+        }
+    }
+}
---
+++ AstarWeb/Models/PoleModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AstarWeb.Models
+{
+    public class PoleModel
+    {
+        public int Id { get; set; }
+        public bool Otwarty { get; set; } //otwarty czy zamknięty (wokable)
+        public string Rodzic { get; set; }
+        public int G { get; set; } //droga po między wierzchołkiem początkowym a x(tym)
+        public int H { get; set; } //przewidywana przez heurystykę droga od x do wierzchołka docelowego
+        public int F { get; set; } //funkcja F(x)=g(x)+h(x)
+    }
+}
